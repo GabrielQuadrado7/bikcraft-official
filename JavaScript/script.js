785834038379c2b50f4b0f79f377414aea8b110a
GabrielQuadrado7 <gabrielquadrado70@gmail.com>
2024-04-27 12:52:48 -0300

    Aulas de JavaScript

---
+++ JavaScript/script.js
@@ -0,0 +1,9 @@
+//const nome = "JavaScript"
+
+//console.log(nome);
+
+const nav = document.querySelector("nav");
+const produtos = document.querySelector(".produtos a");
+
+console.log (nav);
+console.log(produtos);
