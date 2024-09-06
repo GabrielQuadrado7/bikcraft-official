694a47f00a1aae7d2a4c3f70528f261b79ee2f87
GabrielQuadrado7 <gabrielquadrado7@gmail.com>
2024-09-06 14:55:59 -0300

    Ativação dos itens do Orçamento, habilitando parametros da URL tipo e produto

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
-const links = document.querySelectorAll('.header-menu a');
-
 console.log("javascript".includes("a")); //Método que verifica se a palavra existe nesta string ou não. Caso sim retorna TRUE
+
+// Ativar Links do Menu
+const links = document.querySelectorAll('.header-menu a');
 
 function ativarLink(link){
     const url = location.href;
@@ -12,3 +13,17 @@ function ativarLink(link){
 }   	
 
 links.forEach(ativarLink);
+
+// Ativar Items do Orçamento, tipo e produto, extraindo diretamente os parametros da URL
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+    const elemento = document.getElementById(parametro);
+    if(elemento) {
+        elemento.checked = true;
+    }
+}
+
+parametros.forEach(ativarProduto);
+
